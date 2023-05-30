92b22fac2c23e0b269f2953bc00ac9935bfb1c26
MichelAltmann <michel.altmann05@gmail.com>
2023-05-30 01:28:34 -0300

    Signup Activity code Refactor

--- CMChat/app/src/main/java/com/cmchat/ui/login/signup/SignUpActivity.kt
+++ CMChat/app/src/main/java/com/cmchat/ui/login/signup/SignUpActivity.kt
@@ -8,6 +8,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.DatePicker
+import android.widget.EditText
 import android.widget.Toast
 import com.cmchat.DatePickerFragment
 import com.cmchat.cmchat.databinding.ActivitySignUpBinding
@@ -32,10 +33,24 @@ class SignUpActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
     private val dateFormat = SimpleDateFormat(format, Locale.getDefault())
 
     private val viewModel by viewModel<SignupViewModel>()
+
+    private lateinit var usernameEt : EditText
+    private lateinit var emailEt : EditText
+    private lateinit var birthdateEt : EditText
+    private lateinit var passwordEt : EditText
+    private lateinit var repeatPasswordEt : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        usernameEt = binding.signUpUsernameInput
+        emailEt = binding.signUpEmailInput
+        birthdateEt = binding.signUpBirthdateInput
+        passwordEt = binding.signUpPasswordInput
+        repeatPasswordEt = binding.signUpRepeatPasswordInput
 
         onSignUpClick()
 
@@ -56,19 +71,18 @@ class SignUpActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
         viewModel.error.observe(this){
             when (it.message){
                 "Email already in use" -> {
-                    binding.signUpEmailInput.error = "Email already in use"
-                    binding.signUpEmailInput.requestFocus()
+                    emailEt.error = "Email already in use"
+                    emailEt.requestFocus()
                 }
                 "Username already in use" -> {
-                    binding.signUpUsernameInput.error = "Username already in use"
-                    binding.signUpUsernameInput.requestFocus()
+                    emailEt.error = "Username already in use"
+                    emailEt.requestFocus()
                 }
             }
         }
     }
 
     private fun onBirthdateText() {
-        val birthdateEt = binding.signUpBirthdateInput
         birthdateEt.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
@@ -103,7 +117,7 @@ class SignUpActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
     }
 
     private fun updateBirthdate() {
-        binding.signUpBirthdateInput.setText(dateFormat.format(calendar.time))
+        birthdateEt.setText(dateFormat.format(calendar.time))
     }
 
     private fun onCancelClick() {
@@ -114,48 +128,20 @@ class SignUpActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
 
     fun onSignUpClick() {
-        val usernameEt = binding.signUpUsernameInput
-        val emailEt = binding.signUpEmailInput
-        val birthdateEt = binding.signUpBirthdateInput
-        val passwordEt = binding.signUpPasswordInput
-        val repeatPasswordEt = binding.signUpRepeatPasswordInput
         binding.signUpBtnSignup.setOnClickListener {
             val error = "Insert a valid value."
-            if (usernameEt.text.isNullOrEmpty()){
-                usernameEt.error = error
-                usernameEt.requestFocus()
-                return@setOnClickListener
-            }
 
-            if (emailEt.text.isNullOrEmpty()){
-                emailEt.error = error
-                emailEt.requestFocus()
-                return@setOnClickListener
-            }
+            if (dontHaveUsername(usernameEt, error)) return@setOnClickListener
 
-            if (birthdateEt.text.isNullOrEmpty()){
-                birthdateEt.error = error
-                birthdateEt.requestFocus()
-                return@setOnClickListener
-            }
+            if (dontHaveEmail(emailEt, error)) return@setOnClickListener
 
-            if (passwordEt.text.isNullOrEmpty()){
-                passwordEt.error = error
-                passwordEt.requestFocus()
-                return@setOnClickListener
-            }
+            if (dontHaveBirthdate(birthdateEt, error)) return@setOnClickListener
 
-            if (repeatPasswordEt.text.isNullOrEmpty()){
-                repeatPasswordEt.error = error
-                repeatPasswordEt.requestFocus()
-                return@setOnClickListener
-            }
+            if (dontHavePassword(passwordEt, error)) return@setOnClickListener
 
-            if (!passwordEt.text.toString().equals(repeatPasswordEt.text.toString())){
-                passwordEt.error = "Passwords doesn't match."
-                passwordEt.requestFocus()
-                return@setOnClickListener
-            }
+            if (dontHaveRepeatPassword(repeatPasswordEt, error)) return@setOnClickListener
+
+            if (passwordsDontMatch(passwordEt, repeatPasswordEt)) return@setOnClickListener
 
             val username = usernameEt.text.toString()
             val email = emailEt.text.toString()
@@ -163,18 +149,79 @@ class SignUpActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
             val currentDay = Calendar.getInstance()
             val createDate = Date(currentDay.timeInMillis)
-            // Hashing the password
-            val crudePassword = passwordEt.text.toString()
-            val md = MessageDigest.getInstance("SHA-256")
-            val bigPassword = BigInteger(1, md.digest(crudePassword.toByteArray()))
-            val password = bigPassword.toString()
 
+            // Hashing the password
+            val password = passwordHasher(passwordEt)
 
             val user = User(0, email, username,password, date, null, null, null, createDate, 0, 0)
 
-            Log.i(TAG, "onSignUpClick: " + user.birthday.toString())
             viewModel.signup(user)
         }
+    }
+
+    private fun passwordHasher(passwordEt: EditText): String {
+        val crudePassword = passwordEt.text.toString()
+        val md = MessageDigest.getInstance("SHA-256")
+        val bigPassword = BigInteger(1, md.digest(crudePassword.toByteArray()))
+        val password = bigPassword.toString()
+        return password
+    }
+
+    private fun passwordsDontMatch(
+        passwordEt: EditText,
+        repeatPasswordEt: EditText
+    ): Boolean {
+        if (!passwordEt.text.toString().equals(repeatPasswordEt.text.toString())) {
+            passwordEt.error = "Passwords doesn't match."
+            passwordEt.requestFocus()
+            return true
+        }
+        return false
+    }
+
+    private fun dontHaveRepeatPassword(repeatPasswordEt: EditText, error: String): Boolean {
+        if (repeatPasswordEt.text.isNullOrEmpty()) {
+            repeatPasswordEt.error = error
+            repeatPasswordEt.requestFocus()
+            return true
+        }
+        return false
+    }
+
+    private fun dontHavePassword(passwordEt: EditText, error: String): Boolean {
+        if (passwordEt.text.isNullOrEmpty()) {
+            passwordEt.error = error
+            passwordEt.requestFocus()
+            return true
+        }
+        return false
+    }
+
+    private fun dontHaveBirthdate(birthdateEt: EditText, error: String): Boolean {
+        if (birthdateEt.text.isNullOrEmpty()) {
+            birthdateEt.error = error
+            birthdateEt.requestFocus()
+            return true
+        }
+        return false
+    }
+
+    private fun dontHaveEmail(emailEt: EditText, error: String): Boolean {
+        if (emailEt.text.isNullOrEmpty()) {
+            emailEt.error = error
+            emailEt.requestFocus()
+            return true
+        }
+        return false
+    }
+
+    private fun dontHaveUsername(usernameEt: EditText, error: String): Boolean {
+        if (usernameEt.text.isNullOrEmpty()) {
+            usernameEt.error = error
+            usernameEt.requestFocus()
+            return true
+        }
+        return false
     }
 
     override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
